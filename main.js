943571dd8bfa3e8651c9af98d5bedd22a395f84d
Bernvakaris <140438433+Bernvakaris@users.noreply.github.com>
2023-07-25 18:42:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.getElementById("quizForm").addEventListener("submit", function(event) {
     var answer = document.getElementById("answer").value;
 
     // Patikrinti, ar atsakymas teisingas (pavyzdžiui, atitinka kokį nors žodį)
-    if (answer.trim().toLowerCase() === "teisingasatsakymas") {
+    if (answer.trim().toLowerCase() === "butelis") {
         // Nukreipkite į kitą puslapį
         window.location.href = "kitas_puslapis.html";
     } else {
